f1935a2c78c1782dcd21e32008f7538de33f5c5b
MaoHuang2020 <hannahmaohuang@gmail.com>
2021-01-11 11:05:35 -0500

    Reorder_Pedigree_Correctly_866_individuals

--- TraitAnalyses201003/ReorderPedigree/Redo_Pedigree.R
+++ TraitAnalyses201003/ReorderPedigree/Redo_Pedigree.R
@@ -1,16 +1,18 @@
 # Re do the pedigree matrix
 save(GPsequenced,kelpNameColumns,SPGPs,file="What_has_been_Used_from_Step1_Create_Matrix.Rdata")
-
+load("what_has_been_used_from_step1_create_matrix.Rdata")
 ##### Re-order the pedigree matrix
-setwd("/Users/maohuang/Desktop/Kelp/SugarKelpBreeding/TraitAnalyses201003/Making_haploid_CovComb/InputData")
+setwd("/Users/maohuang/Desktop/Kelp/SugarKelpBreeding/TraitAnalyses201003/ReorderPedigree")
 biphasicPedNH<-read.csv("biphasiPedNH_addmoreGP_866.csv",sep=",",header=TRUE,row.names=1)
 
 
-###1 GP genotyped,crossed and had phenotypic data in dataNHpi
-###2 GP not genotyped?,crossed and had phenotypic data in dataNHpi
+###1 GP genotyped,crossed and had SP phenotypic data in dataNHpi
+###2 GP not genotyped?,crossed and had SP phenotypic data in dataNHpi
 
-###3 GP genotyped,not crossed and no phenotypic data in dataNHpi
+###3 GP genotyped,not crossed and no SP phenotypic data in dataNHpi
 ###4 GP from 2019S_plot level
+
+###5 other GP had biomass,not genotyped and not crossed
 
 #### "GPsequenced" has the list of GPs being sequenced in 3 plates
 #### "kelpNameColumns" has the list of unique femaPar x malePar, GPs made cross and had phenotypic data
@@ -90,12 +92,32 @@ str(S_GP_its_parentalGP)
 
 #For 4, all S_GP_its_fnders were already included
 #For 1, to row64, For 2 row65 to row70, For 3 row71 to row93)
-#### List of fnders for 1,2,3,4
+#For 5, row 94 to row 104
 
-All_fnders<-c(GP_seq_fndr,GP_Noseq_fndr,GP_noPheno_fndr,S_GP_its_fnders)  # 189
-str(All_fnders)
-All_fnders_unique<-unique(All_fnders)   #93
-str(All_fnders_unique)
+#### List of fnders for 1,2,3,4
+All_fnders1<-c(GP_seq_fndr,GP_Noseq_fndr,GP_noPheno_fndr,S_GP_its_fnders)  # 189
+str(All_fnders1)
+
+
+###!!! 5. Adding GPs has biomass, but not in the field data (photo score >1) nor in the genotyped list
+GP_Add<-read.csv("AddGP_to_Pedi.csv",sep=",",header=T)
+colnames(GP_Add)<-"WBiomass_Name_InPheno"
+  head(GP_Add)
+  dim(GP_Add)
+GP_Add_unique<-unique(as.character(GP_Add$WBiomass_Name_InPheno)) # 105
+  dim(GP_Add_unique)
+  str(GP_Add_unique)
+
+# find their founders
+# themselves
+BiomassGP_fndr<-find_fnder_for_GP(GP_Add_unique)  
+str(BiomassGP_fndr[!BiomassGP_fndr%in%All_fnders_unique])
+
+All_fnders2<-c(All_fnders1,BiomassGP_fndr) #249
+
+#### List of fnders for 1,2,3,4,5
+All_fnders_unique<-unique(All_fnders2)   #104
+  str(All_fnders_unique)                    
 
 ## I. Ped for fnders
 nFounders<-length(All_fnders_unique) 
@@ -104,20 +126,28 @@ names(fndRow) <- All_fnders_unique
 fndPed <- cbind(fndRow, 0, 0)   
 
 
+## GP_Add_unique: the Added_GPs that's not in genotyped nor phenotyped but had biomass
 All_GPs123<-c(GP_seq,GP_Noseq,GP_noPheno)
-str(All_GPs123)     # 338, unique 
-length(GP_seq)    # row94 to row252 (94+159-1)
-length(GP_Noseq)  # row253 to row320 (253+68-1)
-length(GP_noPheno) #row 321 to row431 (321+111-1)
-sum(S_GP_its_parentalGP %in%(All_GPs123))==length(S_GP_its_parentalGP)   #All the GPs used for 4 are in the All_GPs123 list
+  str(All_GPs123)     # 338,
+  
+GP_Add_unique<-GP_Add_unique[!GP_Add_unique%in%All_GPs123]  # 105->101
+
+All_GPs<-c(All_GPs123,GP_Add_unique)   # 439
+  str(All_GPs)      # 439
+  length(GP_seq)     # row105 to row263 length(c(All_fnders_unique,GP_seq))
+  length(GP_Noseq)   # row264 to row331 /length(c(All_fnders_unique,GP_seq,GP_Noseq))
+  length(GP_noPheno)  #row332 to row442 /length(c(All_fnders_unique,GP_seq,GP_Noseq,GP_noPheno))
+  length(GP_Add_unique)   #row443 to row 543 /length(c(All_fnders_unique,GP_seq,GP_Noseq,GP_noPheno,GP_Add_unique))
+
+sum(S_GP_its_parentalGP %in%(All_GPs))==length(S_GP_its_parentalGP)   #All the GPs used for 4 are in the All_GPs list
 
 ## II. Ped for GPs #### DO I NEED TO ORDER THEM BY FEMALE firt and then BY MALE ???
-GPsRow <- nFounders + 1:length(All_GPs123)
-names(GPsRow) <- All_GPs123
-founderPar<-sapply(strsplit(All_GPs123, split="-", fixed=T), function(vec) paste(vec[1:3], collapse="-"))
+GPsRow <- nFounders + 1:length(All_GPs)
+names(GPsRow) <- All_GPs
+founderPar<-sapply(strsplit(All_GPs, split="-", fixed=T), function(vec) paste(vec[1:3], collapse="-"))
 GPsPed <- cbind(GPsRow, fndRow[founderPar], NA)
 
-## III. Ped for the SP plots in kelpNamesColumns femaParx malePar          
+## III. Ped for the SP plots in 3, aka photo score >1 plots, aka in kelpNamesColumns femaParx malePar          
 sporProg1 <- 1:nrow(kelpNameColumns) + nrow(fndPed) + nrow(GPsPed)
 names(sporProg1)<-paste0(kelpNameColumns[,1],"x",kelpNameColumns[,2])
 progPed1 <- cbind(sporProg1, GPsRow[kelpNameColumns[,1]], GPsRow[kelpNameColumns[,2]]) # row 432 to row 675 (431+244)           
@@ -140,8 +170,13 @@ names(SP_GP_row)<-SPGPs$GametophyteID
 SP_GP_Ped<-cbind(SP_GP_row,spRows[as.character(SPGPs$SPCross)],NA)
 head(SP_GP_Ped)
 
+
+
+
 Ped_in_Order<-rbind(fndPed,GPsPed,progPed,SP_GP_Ped)  
 dim(Ped_in_Order)
-write.csv(Ped_in_Order,"Ped_in_Order_754_Individuals.csv")
+write.csv(Ped_in_Order,"Ped_in_Order_866_Individuals.csv")
 
 
+
+  
